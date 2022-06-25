d844c0d0915c451a7016f3203807333d130aa2d8
yahayaohinoyi <yahaya.suleiman2204@gmail.com>
2022-06-25 04:38:29 +0100

    fix
    m,
    
    djkcnda
    
    ej d
    
    fndf
    
    fjbaf
    
    test
    
    enwld
    
    dj
    
    edj
    
    enlwf
    
    enfe
    
    efe
    
    edkfq
    
    edkfq
    
    eff
    
    eff
    
    ewdnl
    
    ewdnl
    
    ewdnl
    
    ewdnl
    
    ewdnl
    
    ewdnl
    
    wjn
    
    den
    
    wr
    
    fr
    
    fr
    
    fr
    
    fr
    
    rjnf
    
    cen
    
    dnd
    
    ene
    
    fnr
    
    e
    
    efn
    
    e
    
    fekn
    
    dend
    
    ce

--- app/main.js
+++ app/main.js
@@ -1,13 +1,44 @@
 const net = require("net");
+const store = new Map();
+
+function makePingResponse(request) {
+    const DEFAULT = "+PONG\r\n";
+    const COMMAND = request[2];
+    const DATAKEY = request[4];
+    const OK = "+OK\r\n";
+    const EXPIRY = request[8] || '';
+    const EXPIREDBY = EXPIRY ? parseInt(request[10]) + (new Date()).getTime() : Infinity;
+
+    switch(COMMAND) {
+        case 'ping':
+            return "+PONG\r\n";
+        case 'echo':
+            return `+${DATAKEY}\r\n`;
+        case 'set':
+            store.set(DATAKEY, {value: request[6], expiredBy: EXPIREDBY});
+            return OK;
+        case 'get':
+            const result = store.get(DATAKEY);
+            console.log(`${new Date().getTime()} ---- ${result.expiredBy}`)
+            if ((new Date()).getTime() < result.expiredBy) {
+                return `+${result.value}\r\n`;
+            } else {
+                return `$-1\r\n`;
+            }
+        default:
+            return DEFAULT;
+    }
+}
 
 const server = net.createServer((socket) => {
-    socket.write("+PONG\r\n");
-    socket.pipe(socket);
+    socket.on('data', (data) => {
+        const req = data.toString()
+        const array = req.split('\r\n')
+        console.log(array)
+        const pingResponse = makePingResponse(array);
+        socket.write(pingResponse);
+    })
+    socket.on('end', socket.end)
   });
   
   server.listen(6379, "127.0.0.1");
-
-  
-// You can use print statements as follows for debugging, they'll be visible when running tests.
-console.log("Logs from your program will appear here!");
-
