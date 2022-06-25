fd8c7f70ba037e0b2fea77a9512508b38bde2c0b
yahayaohinoyi <yahaya.suleiman2204@gmail.com>
2022-06-25 04:16:28 +0100

    feat: create server

--- app/main.js
+++ app/main.js
@@ -1,5 +1,11 @@
 const net = require("net");
 
+const server = net.createServer((socket) => {
+    socket.pipe(socket);
+  });
+  
+  server.listen(6379, "127.0.0.1");
+
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
 
